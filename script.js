9d8385438e209968911a4d13409a65a9fb7f1d66
la-pamela <155924763+la-pamela@users.noreply.github.com>
2024-12-28 01:44:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evitar envío real del formulario
+    alert("¡Gracias por tu mensaje! Te responderemos pronto.");
+});
